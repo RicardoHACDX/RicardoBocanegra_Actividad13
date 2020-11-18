5d60c1ecec25860e3037f7cb47448b5c630b4d42
RicardoBocanegra <ricardomendez145@gmail.com>
2020-11-18 15:35:47 -0600

    const

---
+++ funciones.js
@@ -0,0 +1,34 @@
+//Suma
+let e=10;
+let f=50;
+console.log (e + f)
+//Resta
+let r=4000;
+let j=2345;
+console.log (r - j)
+//Mayusculas y Minusculas
+ const mensaje = "La Voz De Tu Cabeza Siempre Tiene El Mismo Volumen";
+ console .log (mensaje);
+ console.log(mensaje.toUpperCase);
+ console.log(mensaje.toLocaleLowerCase);
+ //Quitando Espacios vacios
+ const texto = "La tierra no es una esfera perfecta";
+ console.log (texto);
+ console.log(texto.trim());
+ //Separacion
+ const articulos = "Escoba Trapeador Sofá Televisor Radio";
+ const separacion = articulos.split("");
+ console.log(articulos);
+ console.log(separacion);
+ //Correo
+ const email = "ricardomendez@gmail.com";
+ mail= email.split("@")
+ servidor= mail[1];
+ document.write(servidor);
+ console.table(servidor);
+
+
+ 
+
+
+
